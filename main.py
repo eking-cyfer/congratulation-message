e050864f4560cfe472f39f6443ece0ba83f9600f
eking-cyfer <edwardafatsawo20@gmail.com>
2024-12-14 17:10:07 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Input variables
+name = "Edward"
+achievement = "winning the coding contest"
+year = 2024
+
+# Create the message
+message = (
+    "Congratulations " + name + "! " +
+    "Your achievement of " + achievement + " in " +
+    str(year) + " is truly commendable."
+)
+
+# Display the congratulatory message
+print(message)
